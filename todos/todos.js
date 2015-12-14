c0a4c1c703404da42d0b69c9158f483bcef27670
MKS-students <mks.students@hackreactor.com>
2015-12-14 16:55:54 -0600

    Fixing hide completed bug

--- todos/todos.js
+++ todos/todos.js
@@ -8,7 +8,7 @@ if (Meteor.isClient) {
     function($scope, $meteor) {
 
       $scope.tasks = $meteor.collection(function() {
-        return Tasks.find({}, { sort: { createdAt: -1} })
+        return Tasks.find($scope.getReactively('query'), {sort: { createdAt: -1} })
       });
 
       $scope.addTask = function(newTask) {
@@ -16,6 +16,17 @@ if (Meteor.isClient) {
           text:newTask,
           createdAt: new Date()
         });
+      };
+
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideComplted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          scope.query = {};
+      });
+
+      $scope.incompleteCount = function() {
+        return Tasks.find({ checked: {$ne: true} }).count();
       };
 
     }]);
