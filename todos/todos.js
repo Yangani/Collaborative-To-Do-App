268843aef5cd460ecb4f905079e527b42f5762d0
MKS-students <mks.students@hackreactor.com>
2015-12-14 19:50:51 -0600

    Completed MVP

--- todos/todos.js
+++ todos/todos.js
@@ -1,42 +1,68 @@
-Tasks = new Mongo.Collection('tasks');
+// Tasks = new Mongo.Collection('tasks');
 
-if (Meteor.isClient) {
+// if (Meteor.isClient) {
 
-  // Accounts.ui.config({
-  //   passwordSignupFields: "USERNAME ONLY"
-  // });
+//   Accounts.ui.config({
+//     passwordSignupFields: "USERNAME ONLY"
+//   });
 
-  angular.module('todos',['angular-meteor', 'accounts.ui']);
+//   angular.module('todos',['angular-meteor', 'accounts.ui']);
 
-  angular.module('todos').controller('TodosListCtrl', ['$scope', '$meteor',
-    function($scope, $meteor) {
+//   angular.module('todos').controller('TodosListCtrl', ['$scope', '$meteor',
+//     function($scope, $meteor) {
 
-      $scope.tasks = $meteor.collection(function() {
-        return Tasks.find($scope.getReactively('query'), {sort: { createdAt: -1} })
-      });
+//       $scope.tasks = $meteor.collection(function() {
+//         return Tasks.find($scope.getReactively('query'), {sort: { createdAt: -1} })
+//       });
 
-      $scope.addTask = function(newTask) {
-        $scope.tasks.push({
-          text:newTask,
-          createdAt: new Date()
-        });
-      };
+//       $scope.addTask = function(newTask) {
+//         $meteor.call('addTask', newTask);
+//       };
 
-      $scope.$watch('hideCompleted', function() {
-        if ($scope.hideCompleted)
-          $scope.query = {checked: {$ne: true}};
-        else
-          scope.query = {};
-      });
+//       $scope.deleteTask = function(task) {
+//         $meteor.call('deleteTask', task._id);
+//       };
 
-      $scope.incompleteCount = function() {
-        return Tasks.find({ checked: {$ne: true} }).count();
-      };
+//       $setChecked.setChecked = function(task) {
+//         $meteor.call('setChecked', task._id, !task.checked);
+//       };
 
-    }]);
-}
+//       $scope.$watch('hideCompleted', function() {
+//         if ($scope.hideCompleted)
+//           $scope.query = {checked: {$ne: true}};
+//         else
+//           scope.query = {};
+//       });
 
+//       $scope.incompleteCount = function() {
+//         return Tasks.find({ checked: {$ne: true} }).count();
+//       };
 
+//     }]);
+// }
+
+// Meteor.methods({
+//   addTask: function(text) {
+//     if (!Meteor.userId()) {
+//       throw new Meteor.Error('not-athorized');
+//     }
+
+//     Tasks.insert({
+//       text: text,
+//       createdAt: new Date(),
+//       owner:Meteor.userId(),
+//       username: meteor.user().username
+//     });
+//   },
+
+//   deleteTask: function(taskId) {
+//     Task.remove(newTask);
+//   },
+
+//   setChecked: function(taskId, setChecked) {
+//     Tasks.update(taskId, {$set: setChecked});
+//   }
+// });
 
 
  /* // counter starts at 0
@@ -62,3 +88,117 @@ if (Meteor.isServer) {
   });*/
 
 //275px; font size
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  // This code only runs on the client
+  angular.module('simple-todos',['angular-meteor', 'accounts.ui']);
+
+  function onReady() {
+    angular.bootstrap(document, ['simple-todos']);
+  }
+
+  if (Meteor.isCordova)
+    angular.element(document).on('deviceready', onReady);
+  else
+    angular.element(document).ready(onReady);
+
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
+    function ($scope, $meteor) {
+
+      $scope.$meteorSubscribe('tasks');
+
+      $scope.tasks = $meteor.collection(function() {
+        return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
+      });
+
+      $scope.addTask = function (newTask) {
+        $meteor.call('addTask', newTask);
+      };
+
+      $scope.deleteTask = function (task) {
+        $meteor.call('deleteTask', task._id);
+      };
+
+      $scope.setChecked = function (task) {
+        $meteor.call('setChecked', task._id, !task.checked);
+      };
+
+      $scope.setPrivate = function (task) {
+        $meteor.call('setPrivate', task._id, ! task.private);
+      };
+
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideCompleted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          $scope.query = {};
+      });
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
+      };
+
+    }]);
+}
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
+
+if (Meteor.isServer) {
+  Meteor.publish('tasks', function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
